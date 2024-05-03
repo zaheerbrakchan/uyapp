7b92614ce73efa35ae5003f515f6641d804bb6fc
zaheer <xaheer3scc@gmail.com>
2024-05-03 07:10:15 +0530

    added swaager UI

--- src/main/java/com/uyapp/UyappApplication.java
+++ src/main/java/com/uyapp/UyappApplication.java
@@ -3,7 +3,9 @@ package com.uyapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class UyappApplication {
 
 	public static void main(String[] args) {
