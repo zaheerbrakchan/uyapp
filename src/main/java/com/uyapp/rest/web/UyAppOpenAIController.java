97d0e5f065b0e67e8bd6f281e8e330b58f288db8
zaheer <xaheer3scc@gmail.com>
2024-05-03 07:39:01 +0530

    added swaager UI

--- src/main/java/com/uyapp/rest/web/UyAppOpenAIController.java
+++ src/main/java/com/uyapp/rest/web/UyAppOpenAIController.java
@@ -8,20 +8,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/uyapp")
 //@CrossOrigin("*")
-@CrossOrigin(origins = "http://uyapp-production.up.railway.app/*")
+@CrossOrigin(origins = "http://uyapp-production.up.railway.app")
 public class UyAppOpenAIController {
 
     @Autowired
     UyAppOpenAIService uyAppOpenAIService;
 
-    @CrossOrigin(origins = "http://uyapp-production.up.railway.app/*")
+    @CrossOrigin(origins = "http://uyapp-production.up.railway.app")
     @GetMapping("/user-query")
     public String QueryAI(@RequestBody String query) {
         return uyAppOpenAIService.chat(query);
     }
 
 
-    @CrossOrigin(origins = "http://uyapp-production.up.railway.app/*")
+    @CrossOrigin(origins = "http://uyapp-production.up.railway.app")
     @GetMapping("/test")
     public String Test() {
         return "Service is Running !!!!!!!!11";
