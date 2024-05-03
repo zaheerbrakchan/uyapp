0f248f9b59adadce79545aac9a069a520a7494da
zaheer <xaheer3scc@gmail.com>
2024-05-03 07:43:29 +0530

    added swaager UI

--- src/main/java/com/uyapp/rest/web/UyAppOpenAIController.java
+++ src/main/java/com/uyapp/rest/web/UyAppOpenAIController.java
@@ -14,14 +14,14 @@ public class UyAppOpenAIController {
     @Autowired
     UyAppOpenAIService uyAppOpenAIService;
 
-    @CrossOrigin(origins = "http://uyapp-production.up.railway.app")
+
     @GetMapping("/user-query")
     public String QueryAI(@RequestBody String query) {
         return uyAppOpenAIService.chat(query);
     }
 
 
-    @CrossOrigin(origins = "http://uyapp-production.up.railway.app")
+
     @GetMapping("/test")
     public String Test() {
         return "Service is Running !!!!!!!!11";
